05d941b35bd8f51fdbf36f38805f572bb0e78651
Simon Fish <si@mon.fish>
2021-10-05 10:32:53 +0100

    Replicate bug

--- app/components/field_component.rb
+++ app/components/field_component.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
 class FieldComponent < ViewComponent::Base
-  attribute :type, :string, validate: { inclusion: { in: %w[text email password text_area] } }
-  attribute :name, :string
-  attribute :value, :string, validate: false
-  attribute :list, :string, validate: false
-  attribute :placeholder, :string, validate: false
   def initialize(type:, name:, value: nil, list: nil, placeholder: nil)
     @type = type
     @name = name
@@ -25,7 +20,7 @@ class FieldComponent < ViewComponent::Base
   end
 
   def call
-    return content_tag :textarea, value, tag_params.except(:type) if type == :text_area
+    return tag :textarea, @value, tag_params.except(:type) if @type == :text_area
 
     tag :input, tag_params
   end
